076d0db21f75ad291e0be775889229e1ea7ce495
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2022-02-26 10:19:01 +0100

    a single letter should not be used twice

--- src/main/java/Wordle.java
+++ src/main/java/Wordle.java
@@ -2,17 +2,24 @@ public record Wordle(String hidden) {
 
     public String guess(String guess) {
         String result = "";
+        boolean[] used = new boolean[hidden.length()];
         for (int i = 0; i < guess.length(); i++) {
             if (guess.codePointAt(i) == hidden.codePointAt(i)) {
                 result += Character.toString(guess.codePointAt(i)).toUpperCase();
+                used[i] = true;
             } else {
                 boolean found = false;
                 for (int j = 0; j < hidden.length(); j++) {
+                    if (used[j]) {
+                        continue;
+                    }
                     if (i != j
                             && guess.codePointAt(j) != hidden.codePointAt(j)
                             && guess.codePointAt(i) == hidden.codePointAt(j)) {
                         result += Character.toString(guess.codePointAt(i));
+                        used[j] = true;
                         found = true;
+                        break;
                     }
                 }
                 if (!found) {
--- src/test/java/WordleTest.java
+++ src/test/java/WordleTest.java
@@ -32,5 +32,16 @@ class WordleTest {
 
         Assertions.assertThat(new Wordle("bbabb")
                 .guess("aaaaa")).isEqualTo("..A..");
+
+        Assertions.assertThat(new Wordle("abbbb")
+                .guess("accca")).isEqualTo("A....");
+        Assertions.assertThat(new Wordle("abbbb")
+                .guess("accaa")).isEqualTo("A....");
+        Assertions.assertThat(new Wordle("aabbb")
+                .guess("accaa")).isEqualTo("A..a.");
+        Assertions.assertThat(new Wordle("aabbb")
+                .guess("aacaa")).isEqualTo("AA...");
+        Assertions.assertThat(new Wordle("aabbb")
+                .guess("cccaa")).isEqualTo("...aa");
     }
 }
